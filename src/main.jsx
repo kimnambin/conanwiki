4a441add0873dc8e469ea3437f6664796894152f
nambin <mkkim044@naver.com>
2025-01-01 20:43:28 +0900

    빌드 폴더 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import store from './redux/store';
-import Ch_list from './component/character/ch_list';
+import Ch_list from './component/character/Ch_list';
 import {
   BrowserRouter as Router,
   Routes,
