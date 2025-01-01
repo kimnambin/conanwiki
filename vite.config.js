1d29b1b04d564dab31499e9b94e3f2846a4adad9
nambin <mkkim044@naver.com>
2025-01-01 21:24:49 +0900

    변경

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/conanWiki/',
+  base: '/conanwiki/',
   build: {
-    outDir: 'dist',
+    outDir: 'build',
   },
 });
