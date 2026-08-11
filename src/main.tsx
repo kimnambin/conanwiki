ada128e020b763a11e21b46aa1613a46e532999d
nambin <mkkim044@naver.com>
2026-08-11 19:26:18 +0900

    fix: GitHub Pages 딥링크 새로고침 404 문제 해결 (BrowserRouter -> HashRouter)
    GitHub Pages는 정적 파일 서버라 /conanwiki/movies 같은 경로를 직접
    새로고침하거나 열면 실제 파일이 없어 404를 반환했음. HashRouter로
    바꿔 라우팅을 해시 프래그먼트 기반으로 전환.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   useLocation,
